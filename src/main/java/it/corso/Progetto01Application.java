200ed9fdc2657d1b4db827c80888986c06c4a77d
fabrivigna <fabrivigna99@gmail.com>
2023-05-17 12:22:28 +0200

    Initial commit

---
+++ src/main/java/it/corso/Progetto01Application.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progetto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progetto01Application.class, args);
+	}
+
+}
