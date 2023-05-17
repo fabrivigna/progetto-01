ba7109a60ab20a1634d5752ddf5ab5acfd45f2d7
felicemanzi <felice.manzi@outlook.com>
2023-05-17 13:00:02 +0200

    Update Prodotto.java

--- src/main/java/it/corso/model/Prodotto.java
+++ src/main/java/it/corso/model/Prodotto.java
@@ -1,5 +1,54 @@
 package it.corso.model;
 
 public class Prodotto {
-
+	private int id;
+    private String marchio;
+    private String modello;
+    private String sistemaOperativo;
+    private String memoria;
+    private double prezzo;
+    private String descrizione;
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMarchio() {
+		return marchio;
+	}
+	public void setMarchio(String marchio) {
+		this.marchio = marchio;
+	}
+	public String getModello() {
+		return modello;
+	}
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+	public String getSistemaOperativo() {
+		return sistemaOperativo;
+	}
+	public void setSistemaOperativo(String sistemaOperativo) {
+		this.sistemaOperativo = sistemaOperativo;
+	}
+	public String getMemoria() {
+		return memoria;
+	}
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
 }
